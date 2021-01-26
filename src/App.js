57910a6563ceb0b6e5cc943362d3b4defa7dbd4d
Rizal Iswandy <=>
2021-01-26 17:08:54 +0800

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { encrypt, decrypt } from "./crypto";
 
 function App() {
+  output = ''
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="container">
+        <h3>ASCII Caesar Chiper</h3>
+        <div class="input">
+          <label for="input">Input</label>
+          <textarea id="input" cols="30" rows="5"></textarea>
+        </div>
+        <div class="output">
+          <label for="output">Output</label>
+          <textarea id="output" cols="30" rows="5">{output}</textarea>
+        </div>
+        <div class="buttons">
+          <button id="encrypt" onClick={output = encrypt("hello")}><div class="plain">Hello</div> <i class="fa fa-long-arrow-right"></i> <div class="chyper">&%$#@</div></button>
+          <button id="decrypt"><div class="chyper">&%$#@</div> <i class="fa fa-long-arrow-right"></i> <div class="plain">Hello</div></button>
+        </div>
+      </div>
     </div>
   );
 }
