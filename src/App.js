c4aeeb591ef7ebed7e0f923b5cf6158147e75ebe
Rizal Iswandy <=>
2021-01-30 14:53:24 +0800

    deploy again

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useRef } from 'react'
 import { encrypt, decrypt } from "./crypto";
 import Header from "./components/Header";
-import TextField from './components/TextField'
+import './TextField.css'
 import './App.css';
 
 function App() {
